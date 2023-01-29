a4b27b62b5efea8a7633d8b5852d2714ecb9328e
song <thdwjd7899@naver.com>
2023-01-29 22:05:18 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계했다.
    생성자와 equals() , hashcode() 등이
    도메인 맥락과 맞게끔

---
+++ project-board/src/main/java/com/board/projectboard/domain/ArticleComment.java
@@ -0,0 +1,60 @@
+package com.board.projectboard.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Article article;
+    @Setter @Column(nullable = false, length = 500) private String content;
+
+    @CreatedDate @Column private LocalDateTime createdAt;
+    @CreatedBy @Column private String createdBy;
+    @LastModifiedDate @Column private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column private String modifiedBy;
+
+    protected ArticleComment() {}
+
+    public ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
