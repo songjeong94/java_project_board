c57f88a033e3f7a9e4bfd50a29c6771fa5ff16ce
song <thdwjd7899@naver.com>
2023-02-03 22:05:19 +0900

    #12 - h2-console 옵션 삭제
    따로 사용하지 않을 거기 떄문에 콘솔도 필요 없다.

--- project-board/src/main/java/com/board/projectboard/ProjectBoardApplication.java
+++ project-board/src/main/java/com/board/projectboard/ProjectBoardApplication.java
@@ -2,12 +2,12 @@ package com.board.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProjectBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectBoardApplication.class, args);
+         SpringApplication.run(ProjectBoardApplication.class, args);
     }
-
 }
